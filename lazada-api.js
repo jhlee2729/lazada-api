92d45ac10213554d62767ff54e5dc8ed64ec8967
jhlee-iMac <wkdgml7644@gmail.com>
2022-09-04 18:12:13 +0900

    update order 수집, order 정보 DB 저장

--- lazada-api.js
+++ lazada-api.js
@@ -107,7 +107,6 @@ const createOrder = () => {
         let sign_method = "sha256";
         let created_after = contents.after;
         let created_before = contents.before;
-        // let created_before = '2022-08-31T02:55:00.000Z';
         let offset = 0;
         let limit = 100;
     
@@ -147,6 +146,8 @@ const createOrder = () => {
                 console.log("count", count);
                 console.log("count_total", count_total);
                 console.log("insertData", insertData.createOrder.length);
+
+                // console.log("create_order_ids",  contents.order_ids);
                 // console.log("orders", response.data.data);
                 
                 if ( count_total-count !== offset) {
@@ -253,24 +254,19 @@ const updateOrder = () => {
         let access_token = syncData.access_token;
         let timestamp = new Date().getTime().toString();
         let sign_method = "sha256";
-        let created_after = contents.after;
-        let created_before = contents.before;
-        // let update_after = "2022-08-30T16:00:00+08:00";
         let update_after = contents.after;
         let update_before = contents.before;
         let offset = 0;
         let limit = 100;
-    
-        // console.log(`update_after ${update_after}, update_before ${update_before}`)
+        // let status = "unpaid"
+        contents.order_ids = []; // order_ids 초기화
 
         const getOrder = () => {
      
             // console.log(`offset: ${offset}, limit :${limit}`)
-            
-            let sign_format = `/orders/getaccess_token${access_token}app_key${app_key}update_after${update_after}update_before${update_before}sign_method${sign_method}timestamp${timestamp}`;
-            
+            let sign_format = `/orders/getaccess_token${access_token}app_key${app_key}limit${limit}offset${offset}sign_method${sign_method}timestamp${timestamp}update_after${update_after}update_before${update_before}`;
             sign_format = sign_format.toString();
-        
+
             let sign = signature(sign_format);
 
             axios({
@@ -282,36 +278,35 @@ const updateOrder = () => {
                     access_token:access_token,
                     sign_method:sign_method,
                     sign: sign,
-                    update_after:update_after,
-                    update_before:update_before
+                    update_after: update_after,
+                    update_before : update_before,
+                    limit:limit,
+                    offset:offset
                 }
             }).then((response) => {
+
+                let count = response.data.data.count;
+                let count_total = response.data.data.countTotal;
+
+                console.log(`count: ${count}, count_total: ${count_total}`)
             
-                console.log("response", response.data);
+                insertData.updateOrder = insertData.updateOrder.concat(response.data.data.orders);
 
+                response.data.data.orders.forEach(element => {
+                    contents.order_ids.push(element.order_id);
+                });
 
-                // let count = response.data.data.count;
-                // let count_total = response.data.data.countTotal;
-                // console.log("insertData", insertData.updateOrder.length);
-                // console.log("response", response.data.data.orders[10]);
+                console.log("count", count);
+                console.log("count_total", count_total);
+                console.log("updateOrder", insertData.updateOrder.length);
 
-                // insertData.updateOrder = insertData.updateOrder.concat(response.data.data.orders);
-
-                // response.data.data.orders.forEach(element => {
-                //     contents.order_ids.push(element.order_id);
-                // })
-
-                // console.log("count", count);
-                // console.log("count_total", count_total);
-                // console.log("orders", response.data.data);
-                
-                // if ( count_total-count !== offset) {
-                //     offset += limit;
-                //     getOrder();
+                if ( count_total-count !== offset) {
+                    offset += limit;
+                    getOrder();
                     
-                // } else {
-                //     resolve();
-                // }
+                } else {
+                    resolve(true);
+                }
                 
             }).catch((err) => {
                 console.log("err", err);
@@ -321,6 +316,78 @@ const updateOrder = () => {
         
         getOrder();
     })
+}
+
+const updateOrderDetails = () => {
+    return new Promise((resolve,reject) => {
+
+        let app_key = syncData.app_key;
+        let access_token = syncData.access_token;
+        let timestamp = new Date().getTime().toString();
+        let sign_method = "sha256";
+        let order_count = contents.order_ids.length;
+
+        let offset = 0;
+        let limit = 100;
+        let start = 0;
+        let end = start + limit;
+
+        const getOrderDetails = (start, end) => {
+
+            let order_ids = JSON.stringify(contents.order_ids.slice(start, end));
+
+            let sign_format = `/orders/items/getaccess_token${access_token}app_key${app_key}order_ids${order_ids}sign_method${sign_method}timestamp${timestamp}`;
+            sign_format = sign_format.toString();
+
+            let sign = signature(sign_format);
+
+            axios({
+                method: 'GET',
+                url : syncData.endpoints+'/orders/items/get',
+                params: {
+                    app_key: app_key,
+                    timestamp: timestamp,
+                    access_token:access_token,
+                    sign_method:sign_method,
+                    sign: sign,
+                    order_ids: order_ids
+                }
+            }).then((response) => {
+
+                console.log("================Response Length===========", response.data.data.length);
+                response.data.data.forEach(element => {
+                    element.order_items.forEach(i => {
+                        insertData.updateOrderDetails = insertData.updateOrderDetails.concat(i);
+                    })
+                })
+
+                insertData.updateOrderDetails.reverse();
+                console.log("====updateOrderDetails====", insertData.updateOrderDetails.length);
+                
+                callAPI();
+
+            }).catch((err) => {
+                console.log("err", err);
+                resolve(false);
+            })
+        }
+
+        const callAPI = () => {
+
+            offset = limit + offset;
+            start = start + limit;
+            end = end + limit;
+
+            if ( order_count > offset ) {
+                getOrderDetails(start, end)
+            } else {
+                resolve(true)
+            }
+
+        }
+        getOrderDetails(start,end);
+
+    });
 }
 
 const databaseOrderInsert = (order, callback) => {
@@ -497,6 +564,196 @@ const insertOrderDetails = () => {
     })
 }
 
+const databaseOrderEdit = (order, callback) => {
+
+    // order
+    execute(`INSERT INTO app_lazada_order
+    (
+        voucher_platform,
+        voucher,
+        warehouse_code,
+        order_number,
+        voucher_seller,
+        created_at,
+        voucher_code,
+        gift_option,
+        shipping_fee_discount_platform,
+        customer_last_name,
+        promised_shipping_times,
+        updated_at,
+        price,
+        national_registration_number,
+        shipping_fee_original,
+        payment_method,
+        customer_first_name,
+        shipping_fee_discount_seller,
+        shipping_fee,
+        branch_number,
+        tax_code,
+        items_count,
+        delivery_info,
+        statuses,
+        address_billing_country,
+        address_billing_address3,
+        address_billing_phone,
+        address_billing_address2,
+        address_billing_city,
+        address_billing_address1,
+        address_billing_post_code,
+        address_billing_phone2,
+        address_billing_last_name,
+        address_billing_address5,
+        address_billing_address4,
+        address_billing_first_name,
+        extra_attributes,
+        order_id,
+        remarks,
+        gift_message,
+        address_shipping_country,
+        address_shipping_address3,
+        address_shipping_phone,
+        address_shipping_address2,
+        address_shipping_city,
+        address_shipping_address1,
+        address_shipping_post_code,
+        address_shipping_phone2,
+        address_shipping_last_name,
+        address_shipping_address5,
+        address_shipping_address4,
+        address_shipping_first_name,
+        market
+    )
+    VALUES
+    (
+        "${order.voucher_platform}",
+        "${order.voucher}",
+        "${order.warehouse_code}",
+        "${order.order_number}",
+        "${order.voucher_seller}",
+        "${order.created_at}",
+        "${order.voucher_code}",
+        "${order.gift_option}",
+        ${Number(order.shipping_fee_discount_platform)},
+        "${order.customer_last_name}",
+        "${order.promised_shipping_times}",
+        "${order.updated_at}",
+        ${Number(order.price)},
+        "${order.national_registration_number}",
+        ${Number(order.shipping_fee_original)},
+        "${order.payment_method}",
+        "${order.customer_first_name}",
+        "${order.shipping_fee_discount_seller}",
+        ${Number(order.shipping_fee)},
+        "${order.branch_number}",
+        "${order.tax_code}",
+        ${Number(order.items_count)},
+        "${order.delivery_info}",
+        "${order.statuses[0]}",
+        "${order.address_billing.country}",
+        "${order.address_billing.address3}",
+        "${order.address_billing.phone}",
+        "${order.address_billing.address2}",
+        "${order.address_billing.city}",
+        "${order.address_billing.address1}",
+        "${order.address_billing.post_code}",
+        "${order.address_billing.phone2}",
+        "${order.address_billing.last_name}",
+        "${order.address_billing.address5}",
+        "${order.address_billing.address4}",
+        "${order.address_billing.first_name}",
+        "${order.extra_attributes.replace(/"/g, '\\"')}",
+        "${order.order_id}",
+        "${order.remarks}",
+        "${order.gift_message}",
+        "${order.address_shipping.country}",
+        "${order.address_shipping.address3}",
+        "${order.address_shipping.phone}",
+        "${order.address_shipping.address2}",
+        "${order.address_shipping.city}",
+        "${order.address_shipping.address1}",
+        "${order.address_shipping.post_code}",
+        "${order.address_shipping.phone2}",
+        "${order.address_shipping.last_name}",
+        "${order.address_shipping.address5}",
+        "${order.address_shipping.address4}",
+        "${order.address_shipping.first_name}",
+        "${contents.country}"
+    ) ON DUPLICATE KEY UPDATE
+        voucher_platform = "${order.voucher_platform}",
+        voucher = "${order.voucher}", 
+        warehouse_code = "${order.warehouse_code}", 
+        voucher_seller = "${order.voucher_seller}",
+        created_at = "${order.created_at}",
+        voucher_code = "${order.voucher_code}",
+        gift_option = "${order.gift_option}",
+        shipping_fee_discount_platform = ${Number(order.shipping_fee_discount_platform)},
+        customer_last_name = "${order.customer_last_name}",
+        promised_shipping_times = "${order.promised_shipping_times}",
+        updated_at = "${order.updated_at}",
+        price = ${Number(order.price)},
+        national_registration_number = "${order.national_registration_number}",
+        shipping_fee_original = ${Number(order.shipping_fee_original)},
+        payment_method = "${order.payment_method}", 
+        customer_first_name = "${order.customer_first_name}",
+        shipping_fee_discount_seller = "${order.shipping_fee_discount_seller}",
+        shipping_fee = ${Number(order.shipping_fee)},
+        branch_number = "${order.branch_number}",
+        tax_code = "${order.tax_code}",
+        items_count = ${Number(order.items_count)},
+        delivery_info = "${order.delivery_info}",
+        statuses = "${order.statuses[0]}",
+        address_billing_country = "${order.address_billing.country}",
+        address_billing_address3 = "${order.address_billing.address3}",
+        address_billing_phone = "${order.address_billing.phone}",
+        address_billing_address2 = "${order.address_billing.address2}",
+        address_billing_city = "${order.address_billing.city}",
+        address_billing_address1 = "${order.address_billing.address1}",
+        address_billing_post_code = "${order.address_billing.post_code}",
+        address_billing_phone2 = "${order.address_billing.phone2}",
+        address_billing_last_name = "${order.address_billing.last_name}",
+        address_billing_address5 = "${order.address_billing.address5}",
+        address_billing_address4 = "${order.address_billing.address4}",
+        address_billing_first_name = "${order.address_billing.first_name}",
+        remarks = "${order.remarks}",
+        gift_message = "${order.gift_message}",
+        address_shipping_country = "${order.address_shipping.country}",
+        address_shipping_address3 = "${order.address_shipping.address3}",
+        address_shipping_phone = "${order.address_shipping.phone}",
+        address_shipping_address2 = "${order.address_shipping.address2}",
+        address_shipping_city = "${order.address_shipping.city}",
+        address_shipping_address1 = "${order.address_shipping.address1}",
+        address_shipping_post_code = "${order.address_shipping.post_code}",
+        address_shipping_phone2 = "${order.address_shipping.phone2}",
+        address_shipping_last_name = "${order.address_shipping.last_name}",
+        address_shipping_address5 = "${order.address_shipping.address5}",
+        address_shipping_address4 = "${order.address_shipping.address4}",
+        address_shipping_first_name = "${order.address_shipping.first_name}",
+        market = "${contents.country}"
+    `,
+
+    (err,rows)=>{
+        if ( err ) {
+            throw err;
+        } else {
+            callback();
+        }
+    },{});
+}
+
+const editOrder = () => {
+    return new Promise((resolve,reject) => {
+
+        let loop = 0;
+        const callAPI = () => {
+
+            insertData.updateOrder.length == loop ? 
+                resolve() :
+                databaseOrderEdit(insertData.updateOrder[loop++],callAPI);
+        }
+        databaseOrderEdit(insertData.updateOrder[loop++],callAPI);
+    });
+}
+
 const timeSave = () => {
     return new Promise((resolve,reject) => {
 
@@ -524,7 +781,7 @@ const timeSave = () => {
 const connectionClose = (callback,bool) => {
     return new Promise((resolve,reject) => {
 
-        console.log(insertData.createOrder.length, insertData.createOrderDetails.length);
+        console.log(insertData.createOrder.length, insertData.createOrderDetails.length, insertData.updateOrder.length, insertData.updateOrderDetails.length);
         console.log(new Date() + ' 종료');
         console.log('=====================================================================');
         console.timeEnd();
@@ -572,7 +829,12 @@ const worker = async (sync,callback,bool) => {
             success_details_1 = await createOrderDetails();
         }
 
-        // const success2 = await updateOrder();
+        const success2 = await updateOrder();
+        let success_details_2 = true;
+
+        if ( insertData.updateOrder.length != 0 ) {
+            success_details_2 = await updateOrderDetails();
+        }
 
         if ( !success1 ) {
             await connectionClose(callback,bool);
@@ -584,13 +846,26 @@ const worker = async (sync,callback,bool) => {
             return;
         }
 
-        console.log("insertData.createOrder",  insertData.createOrder.length);
-        console.log("createOrderDetails",  insertData.createOrderDetails.length);
+        if ( !success2 ) {
+            await connectionClose(callback,bool);
+            return;
+        }
+
+        if ( !success_details_2 ) {
+            await connectionClose(callback,bool);
+            return;
+        }
+        // console.log("createOrder",  insertData.createOrder.length);
+        // console.log("createOrderDetails",  insertData.createOrderDetails.length);
+        // console.log("updateOrder",  insertData.updateOrder.length);
+        // console.log("updateOrderDetails",  insertData.updateOrderDetails.length);
 
         insertData.createOrder.length !=0 && await insertOrder();
         insertData.createOrder.length !=0 && await insertOrderDetails();
+        insertData.updateOrder.length !=0 && await editOrder();
+        // insertData.updateOrder.length !=0 && await editOrderDetails();
 
-        await timeSave();
+        // await timeSave();
 
         await connectionClose(callback,bool);
 
