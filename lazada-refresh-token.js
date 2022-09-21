f9c40e63f425f3677f9f1f44f33baf2d489586e1
jhlee2729 <wkdgml7644@gmail.com>
2022-09-21 19:01:14 +0900

    access token 재발급 수정

--- lazada-refresh-token.js
+++ lazada-refresh-token.js
@@ -6,7 +6,8 @@ const crypto = require('crypto');
 const axios = require('axios');
 
 const syncData = { 
-    market: '',
+    app_key: '',
+    app_secret:'',
     access_token: '',
     expires_in: 0,
     expires_in_time: '',
@@ -24,9 +25,7 @@ const execute = (sql,callback,data = {})=>{
             connection.release();
 
             if ( err ) {
-                error_hook(syncData.market,err,(e,res) => {
-                    throw err;
-                });
+                throw err;
             } else {
                 callback(err, rows);
             }
@@ -41,28 +40,44 @@ const closing = () => {
 const getLazadaSync = () => {
     return new Promise((resolve,reject)=>{
 
-        execute(`SELECT * FROM app_lazada_sync`,(err,rows) => {
+        execute(`SELECT 
+                    app_key, 
+                    app_secret,
+                    access_token,
+                    expires_in,
+                    expires_in_time,
+                    refresh_token,
+                    refresh_token_expires_in,
+                    refresh_token_expires_in_time 
+                FROM app_lazada_sync
+                GROUP BY app_key, app_secret, access_token, expires_in, expires_in_time, refresh_token, refresh_token_expires_in, refresh_token_expires_in_time`, (err,rows) => {
             if (err) {
                 throw err;
             } else {
 
-                syncData.access_token = rows[0].access_token;
-                syncData.expires_in = rows[0].expires_in;
-                syncData.expires_in_time = rows[0].expires_in_time;
-                syncData.refresh_token = rows[0].refresh_token;
-                syncData.refresh_token_expires_in = rows[0].refresh_token_expires_in;
-                syncData.refresh_token_expires_in_time = rows[0].refresh_token_expires_in_time;
-                resolve();
+                let count = rows.length;
+                let check = 0;
+
+                const goway = () => { 
+
+                    if ( count != check ) {
+                        refreshAccessToken(rows[check++], goway, check==count);
+
+                    } else {
+                        resolve();
+                    }
+                }
+                goway();
             }
         });
     });
 }
 
-const refreshAccessToken = () => {
+const refreshAccessToken = (syncData, callback, bool) => {
     return new Promise((resolve,reject) => {
 
-        let app_key = "111424";
-        let app_secret = "nDF06KuA5wpGuhTqHKePvOUjcVzEHuGa";
+        let app_key = syncData.app_key;
+        let app_secret = syncData.app_secret;
         let refresh_token = syncData.refresh_token;
         let access_token = syncData.access_token;
         let timestamp = new Date().getTime().toString();
@@ -87,17 +102,22 @@ const refreshAccessToken = () => {
     
         }).then((response) => {
 
+            console.log(`app_key : ${app_key}, acess_token ${response.data.expires_in}, refresh_expires_in : ${response.data.refresh_expires_in}`);
+
             let access_token = response.data.access_token;
             let refresh_token = response.data.refresh_token;
             let now = new Date().getTime();
 
-            //access_token
+            //access_token 604800
             let expires_in = response.data.expires_in;
             let expires_in_time = dateformat(now + (expires_in * 1000),'yyyy-mm-dd HH:MM:ss'); // ms * 1000 = sec
             
-            //refresh_token
+            //refresh_token 2592000
             let refresh_token_expires_in = response.data.refresh_expires_in;
             let refresh_token_expires_in_time = dateformat(now + (refresh_token_expires_in * 1000),'yyyy-mm-dd HH:MM:ss');
+
+            // console.log(`====RESPONSE=== ${expires_in}, ${refresh_token_expires_in}========`);
+            // console.log(`========== ${expires_in_time}, ${refresh_token_expires_in_time}========`);
             
             execute(`UPDATE app_lazada_sync
                     SET
@@ -107,11 +127,18 @@ const refreshAccessToken = () => {
                     refresh_token="${refresh_token}",
                     refresh_token_expires_in="${refresh_token_expires_in}",
                     refresh_token_expires_in_time="${refresh_token_expires_in_time}"
+                    WHERE app_key = "${app_key}"
                     `,
                     (err,rows) => {
                         if (err) {
                             throw err;
                         } else {
+
+                            if(!bool){
+                                callback();
+                                return;
+                            }
+
                             resolve();
                             console.log(new Date() + '종료');
                             closing();
@@ -121,16 +148,17 @@ const refreshAccessToken = () => {
             closing();
             console.log(err)            
         })
+
+        resolve();
     })
 }
 
 const worker = async() => { 
 
     try {
+
         console.log(new Date() + '시작');
         await getLazadaSync();
-        await refreshAccessToken();
-
     } catch(e){
         console.log(e)
     }
