de801b051a7542f6e3af5be694c2b0be72564950
jhlee-iMac <wkdgml7644@gmail.com>
2022-09-24 10:49:06 +0900

    getProductItem 상세

--- lazada-getProductItem.js
+++ lazada-getProductItem.js
@@ -1,2 +1,45 @@
 const crypto = require('crypto');
 const axios = require('axios');
+
+const getProductItem = () => {   
+
+    return new Promise((resolve,reject) => {
+
+        const app_key = "111424";
+        const app_secret = "nDF06KuA5wpGuhTqHKePvOUjcVzEHuGa";
+        const sign_method = "sha256";
+        const access_token = "50000200819dSmgr8JAQvRpcb0HtgpR1f83448bgRvH6DEr7MxxjlDwL0vyiSz9D";
+        const timestamp = new Date().getTime().toString();
+        const item_id = "1964688313";
+        
+        let stringformat = `/product/item/getaccess_token${access_token}app_key${app_key}item_id${item_id}sign_method${sign_method}timestamp${timestamp}`;
+        stringformat = stringformat.toString();
+        
+        const sign = crypto.createHmac('sha256', app_secret).update(stringformat).digest('hex').toUpperCase();
+
+        axios({
+
+            method : "GET",
+            url : "https://api.lazada.com.my/rest/product/item/get",
+            params : {
+                access_token : access_token,
+                app_key : app_key,
+                sign_method : sign_method,
+                timestamp : timestamp,
+                item_id: item_id,
+                sign : sign,
+            }
+        })
+        .then((response)=>{
+
+            console.log("response", response.data);
+            
+        })
+        .catch((err) => {
+
+            console.log("err", err);
+        })
+    })
+}
+
+getProductItem();
